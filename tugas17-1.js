69c1c1c266bf0e1df9d229baa4a8121da48c3026
Dwi Yulianto <98dwiyulianto@gmail.com>
2020-10-17 09:10:55 +0700

    tugas 17 JS
    tugas untuk Niomic

---
+++ tugas17-1.js
@@ -0,0 +1,4 @@
+let belajar = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+let memancing = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+
+console.log(belajar.toLowerCase() + '\n' + memancing.toUpperCase());
